8efd1f64efe5adf6fe96bc6748602764e56d4aa4
2020LM <55995013+2020LM@users.noreply.github.com>
2019-10-12 11:34:03 -0400

    Add files via upload

---
+++ pweb.js
@@ -0,0 +1,91 @@
+$(document).ready(function(){
+ 
+	$('.scroll').click(function(){
+		$('body, html').animate({
+			scrollTop: '0px'
+		}, 300);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+ 
+});
+
+$(document).ready(function(){
+ 
+	$('.b1').click(function(){
+		$('body, html').animate({
+			scrollTop: '750px'
+		}, 300);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+ 
+});
+
+$(document).ready(function(){
+ 
+	$('.b2').click(function(){
+		$('body, html').animate({
+			scrollTop: '1850px'
+		}, 300);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+ 
+});
+
+$(document).ready(function(){
+ 
+	$('.b3').click(function(){
+		$('body, html').animate({
+			scrollTop: '2285px'
+		}, 300);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+ 
+});
+
+$(document).ready(function(){
+ 
+	$('.b4').click(function(){
+		$('body, html').animate({
+			scrollTop: '3050px'
+		}, 300);
+	});
+ 
+	$(window).scroll(function(){
+		if( $(this).scrollTop() > 0 ){
+			$('.ir-arriba').slideDown(300);
+		} else {
+			$('.ir-arriba').slideUp(300);
+		}
+	});
+ 
+});
+
+
